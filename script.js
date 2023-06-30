388b4d11320c3833c0331e4b4ebaf47f8a7ac7b6
Walii78 <129457570+Walii78@users.noreply.github.com>
2023-06-29 17:50:00 -0700

    Add project files.

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    var heading = document.querySelector('h1');
+    heading.addEventListener('mouseover', function () {
+        heading.style.color = getRandomColor();
+    });
+};
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
